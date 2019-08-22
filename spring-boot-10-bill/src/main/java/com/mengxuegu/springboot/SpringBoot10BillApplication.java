9373f6d013ccc24dec980b3e1418fbe8f9f86828
史向洋 <1530075477@qq.com>
2019-08-22 23:36:49 +0800

    新增10-bill

---
+++ spring-boot-10-bill/src/main/java/com/mengxuegu/springboot/SpringBoot10BillApplication.java
@@ -0,0 +1,13 @@
+package com.mengxuegu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10BillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot10BillApplication.class, args);
+    }
+
+}
